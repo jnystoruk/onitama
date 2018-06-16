a240d605470bbc61f85324b333ebc51989aea93f
Jonah Nystoruk <janystoruk@gmail.com>
2018-06-16 11:12:49 -0700

    Implemented static method Card.getCardByEnum()

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,5 @@
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Card {
 
@@ -29,14 +29,23 @@ public class Card {
 		return yMoves;
 	}
 
-	public Set<Card> getAllCards() {
+	public static Set<Card> getAllCards() {
 		if (allCards == null) {
 			buildAllCards();
 		}
 		return allCards;
 	}
 
-	private void buildAllCards() {
+	public static Card getCardByEnum(CardEnum e) {
+		for (Card card : getAllCards()) {
+			if (e.name().equals(card.getName())) {
+				return card;
+			}
+		}
+		throw new IllegalStateException("No Card associated with CardEnum " + e.name());
+	}
+
+	private static void buildAllCards() {
 		allCards = new HashSet<>();
 		for (CardEnum card : CardEnum.values()) {
 			allCards.add(new Card(card.name(), card.getXMoves(), card.getYMoves()));
