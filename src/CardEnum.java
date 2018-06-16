e420a096a73ed7d8cfd26f1e265aa52c70ac165c
Jonah Nystoruk <janystoruk@gmail.com>
2018-06-15 17:50:27 -0700

    Fixed improper syntax in CardEnum.java

--- src/CardEnum.java
+++ src/CardEnum.java
@@ -1,52 +1,51 @@
 public enum CardEnum {
 
- Tiger({0, 0}, {2, -1}),
+	Tiger(new int[] { 0, 0 }, new int[] { 2, -1 }),
 
- Dragon({-2, -1, 1, 2}, {1, -1, -1, 1}),
+	Dragon(new int[] { -2, -1, 1, 2 }, new int[] { 1, -1, -1, 1 }),
 
- Frog({-2, -1, 1}, {0, 1, -1}),
+	Frog(new int[] { -2, -1, 1 }, new int[] { 0, 1, -1 }),
 
- Rabbit({-1, 1, 2}, {-1, 1, 0}),
+	Rabbit(new int[] { -1, 1, 2 }, new int[] { -1, 1, 0 }),
 
- Crab({-2, 0, 2}, {0, 1, 0}),
+	Crab(new int[] { -2, 0, 2 }, new int[] { 0, 1, 0 }),
 
- Elephant({-1, -1, 1, 1}, {1, 0, 0, 1}),
+	Elephant(new int[] { -1, -1, 1, 1 }, new int[] { 1, 0, 0, 1 }),
 
- Goose({-1, -1, 1, 1}, {1, 0, 0, -1}),
+	Goose(new int[] { -1, -1, 1, 1 }, new int[] { 1, 0, 0, -1 }),
 
- Rooster({-1, -1, 1, 1}, {-1, 0, 0, 1}),
+	Rooster(new int[] { -1, -1, 1, 1 }, new int[] { -1, 0, 0, 1 }),
 
- Monkey({-1, -1, 1, 1}, {1, -1, 1, -1}),
+	Monkey(new int[] { -1, -1, 1, 1 }, new int[] { 1, -1, 1, -1 }),
 
- Mantis({-1, 0, 1}, {1, -1, 1}),
+	Mantis(new int[] { -1, 0, 1 }, new int[] { 1, -1, 1 }),
 
- Horse({-1, 0, 0}, {0, 1, -1}),
+	Horse(new int[] { -1, 0, 0 }, new int[] { 0, 1, -1 }),
 
- Ox({0, 0, 1}, {1, -1, 0}),
+	Ox(new int[] { 0, 0, 1 }, new int[] { 1, -1, 0 }),
 
- Crane({-1, 0, 1}, {-1, 1, -1}),
+	Crane(new int[] { -1, 0, 1 }, new int[] { -1, 1, -1 }),
 
- Boar({-1, 0, 1}, {0, 1, 0}),
+	Boar(new int[] { -1, 0, 1 }, new int[] { 0, 1, 0 }),
 
- Eel({-1, -1, 1}, {1, -1, 0}),
+	Eel(new int[] { -1, -1, 1 }, new int[] { 1, -1, 0 }),
 
- Cobra({-1, 1, 1}, {0, 1, -1});
-   
+	Cobra(new int[] { -1, 1, 1 }, new int[] { 0, 1, -1 });
 
-    private int[] xMoves;
+	private int[] xMoves;
 
-    private int[] yMoves;
+	private int[] yMoves;
 
-    public CardEnum(int[] xMoves, int[] yMoves){
-        this.xMoves = xMoves;
-        this.yMoves = yMoves;
-    }
+	private CardEnum(int[] xMoves, int[] yMoves) {
+		this.xMoves = xMoves;
+		this.yMoves = yMoves;
+	}
 
-    public int[] getXMoves(){
-        return xMoves;
-    }
+	public int[] getXMoves() {
+		return xMoves;
+	}
 
-    public int[] getYMoves(){
-        return yMoves;
-    }
+	public int[] getYMoves() {
+		return yMoves;
+	}
 }
